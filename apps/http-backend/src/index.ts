917e3da81f8d78c7b73d632c9e3c091892a33d22
Akash Pandey <ap6195296@gmail.com>
2025-08-29 01:08:53 +0530

    creating http backend and websocket backend packages

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/db/client";
+
+
+
+
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return;
+    }
+
+    //db call
+    res.json({
+        userId : "123"
+    })
+   
+})
+
+app.post("/signin", (req, res) =>{
+
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return;
+    }
+    const userID =1;
+    const token = jwt.sign({
+        userID
+    }, JWT_SECRET);
+
+    res.json({ token });
+
+})
+
+app.post("/room",middleware,  (req,res) =>{
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message : "incorrect inputs"
+        })
+        return;
+    }
+    //db call
+
+    res.json({
+        roomid : 123
+    })
+
+})
+
+
+
+app.listen(3001);
