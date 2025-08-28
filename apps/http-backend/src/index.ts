c68728f9e31e94e74ef9e694a75957f35bf678db
Akash Pandey <ap6195296@gmail.com>
2025-08-29 02:09:43 +0530

    completed signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -11,19 +11,32 @@ import {prismaClient} from "@repo/db/client";
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup",async (req, res) => {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             message : "incorrect inputs"
         })
         return;
     }
+    try{
 
-    //db call
-    res.json({
-        userId : "123"
-    })
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name,
+            }
+        })
+        res.json({
+            userId : "123"
+        })
+    }
+    catch(e){
+        res.status(411).json({
+            message : "User Already Exist With this Username"
+        })
+    }
    
 })
 
